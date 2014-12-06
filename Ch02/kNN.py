e51f39d7d43b7f8337d0cc02c0fc142dbbc30dfd
DevSusu <os1742@gmail.com>
2014-12-06 15:24:19 +0900

    added comments to help understanding the code

--- Ch02/kNN.py
+++ Ch02/kNN.py
@@ -16,16 +16,33 @@ import operator
 from os import listdir
 
 def classify0(inX, dataSet, labels, k):
+
+    # inX = the input vector to classify
+    # the hole set of practice examples
+    # labels = all the classification class
+
+    # numpy.ndarray.shape : the dimension of array. ( n,m ) if n*m array.
     dataSetSize = dataSet.shape[0]
     diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    # subtraction between matrix.
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
     distances = sqDistances**0.5
-    sortedDistIndicies = distances.argsort()     
-    classCount={}          
+
+    # Calculating distance ( Euclidian distance )
+
+    # Sorting distances
+    sortedDistIndicies = distances.argsort()
+
+    # classCount Dictionary Dictionary = HashMap
+    classCount={}
     for i in range(k):
+
+        # voteIlabel should be A or B
         voteIlabel = labels[sortedDistIndicies[i]]
+        # Dictionary.get(key,default_value)
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+
     sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
@@ -34,26 +51,45 @@ def createDataSet():
     labels = ['A','A','B','B']
     return group, labels
 
+# For datingTestSet Analysis
 def file2matrix(filename):
     love_dictionary={'largeDoses':3, 'smallDoses':2, 'didntLike':1}
+
+    # Open txt file of Date Information
     fr = open(filename)
+
+    # read lines
     arrayOLines = fr.readlines()
     numberOfLines = len(arrayOLines)            #get the number of lines in the file
     returnMat = zeros((numberOfLines,3))        #prepare matrix to return
     classLabelVector = []                       #prepare labels return   
+
     index = 0
+
     for line in arrayOLines:
+
+        # String.strip() rips off the specific char from start and end
         line = line.strip()
+
+        # returns array of strings splited by the char \t is Tab
         listFromLine = line.split('\t')
+
+        # Storing parsed words
         returnMat[index,:] = listFromLine[0:3]
+
+        # Convert largeDoses, smallDoses, didntLike to 1,2,3 numbers
+        # array[-1] = last element of array
         if(listFromLine[-1].isdigit()):
             classLabelVector.append(int(listFromLine[-1]))
         else:
             classLabelVector.append(love_dictionary.get(listFromLine[-1]))
         index += 1
+
     return returnMat,classLabelVector
 
     
+# for every columns to have the same effect on prediction
+# newValue = ( oldValue - min ) / ( max - min )
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
     maxVals = dataSet.max(0)
@@ -65,32 +101,54 @@ def autoNorm(dataSet):
     return normDataSet, ranges, minVals
    
 def datingClassTest():
-    hoRatio = 0.50      #hold out 10%
+
+    # haRatio = 0.10
+    hoRatio = 0.10      #hold out 10%
+
     datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')       #load data setfrom file
+    # normalize
     normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
+
+    m = normMat.shape[0] # Vertical size of the matrix
+    numTestVecs = int(m*hoRatio) # Test with 50% of the data
+    # This should be Changed to int to avoid float errors
     errorCount = 0.0
+
     for i in range(numTestVecs):
-        classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
+
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+
+        # classify0( inX, dataSet, labels, k)
+        # inX = the input vector to classify
+        # dataSet = the hole set of practice examples
+        # labels = all the classification class
+
         print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
+
         if (classifierResult != datingLabels[i]): errorCount += 1.0
+
     print "the total error rate is: %f" % (errorCount/float(numTestVecs))
     print errorCount
     
 def classifyPerson():
     resultList = ['not at all', 'in small doses', 'in large doses']
-    percentTats = float(raw_input(\
-                                  "percentage of time spent playing video games?"))
+
+    # receiving datas of the new Person
+    # \ (backslash) means nothing. just showing line changes on books.
+    percentTats = float(raw_input("percentage of time spent playing video games?"))
     ffMiles = float(raw_input("frequent flier miles earned per year?"))
     iceCream = float(raw_input("liters of ice cream consumed per year?"))
+
+    # load file, normalize, make a testing vector
     datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
     normMat, ranges, minVals = autoNorm(datingDataMat)
     inArr = array([ffMiles, percentTats, iceCream, ])
-    classifierResult = classify0((inArr - \
-                                  minVals)/ranges, normMat, datingLabels, 3)
+
+    classifierResult = classify0((inArr - minVals)/ranges, normMat, datingLabels, 3)
+
     print "You will probably like this person: %s" % resultList[classifierResult - 1]
-    
+
+# Changes the bitmap image file to a 1*1024 vector
 def img2vector(filename):
     returnVect = zeros((1,1024))
     fr = open(filename)
@@ -104,22 +162,37 @@ def handwritingClassTest():
     hwLabels = []
     trainingFileList = listdir('trainingDigits')           #load the training set
     m = len(trainingFileList)
+
     trainingMat = zeros((m,1024))
+
     for i in range(m):
         fileNameStr = trainingFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        # take off .txt
+        fileStr = fileNameStr.split('.')[0]
+        # figure out what number does the file represent
         classNumStr = int(fileStr.split('_')[0])
         hwLabels.append(classNumStr)
         trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+
     testFileList = listdir('testDigits')        #iterate through the test set
     errorCount = 0.0
     mTest = len(testFileList)
+
     for i in range(mTest):
         fileNameStr = testFileList[i]
-        fileStr = fileNameStr.split('.')[0]     #take off .txt
+        # take off .txt
+        fileStr = fileNameStr.split('.')[0]
+        # figure out what number does the file represent
         classNumStr = int(fileStr.split('_')[0])
+        # making the inX for classify0
         vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+
+        # classify0( inX, dataSet, labels, k)
+        # inX = the input vector to classify
+        # dataSet = the hole set of practice examples
+        # labels = all the classification class
         classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+
         print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr)
         if (classifierResult != classNumStr): errorCount += 1.0
     print "\nthe total number of errors is: %d" % errorCount
